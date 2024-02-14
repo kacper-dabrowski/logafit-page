c9c6214e3ba4b30ad00a4672e31132b83360995a
kacper-dabrowski <kdtw301@gmail.com>
2024-02-14 12:44:19 +0100

    feat(navbar): add basic navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
+import { clsx } from "clsx";
+import { interFont } from "../fonts/inter";
+import { Navbar } from "../layout/navbar/navbar";
 import "./index.scss";
+import styles from "./layout.module.scss";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const classNames = clsx(interFont.className, styles.content);
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={classNames}>
+        <Navbar additionalClasses={styles.navbar} />
+        <main className={styles.content}>{children}</main>
+        <footer className={styles.footer} />
+      </body>
     </html>
   );
 }
