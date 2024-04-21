07856462fba8e047a4ff38e71be1b33b9e6e6d97
kacper-dabrowski <kdtw301@gmail.com>
2024-04-21 23:41:55 +0200

    fix(events): fix events for mobile view

--- src/features/events/card/card.tsx
+++ src/features/events/card/card.tsx
@@ -4,38 +4,63 @@ import styles from "./card.module.scss";
 export function EventCard() {
   return (
     <div className={styles.card}>
-      <div className={styles.badge}>
-        <Image
-          src="/assets/cold-weather-icon.svg"
-          width={18}
-          height={18}
-          alt="śnieżynka"
-        />
-        Zima 2024
+      <div className={styles.topSection}>
+        <h3 className={styles.heading}>Zimowisko - Czarna Góra</h3>
+        <div className={styles.badge}>
+          <Image
+            src="/assets/cold-weather-icon.svg"
+            alt="śniezynka"
+            width={12}
+            height={12}
+          />
+          Zima 2024
+        </div>
       </div>
-      <div className={styles.contentWrapper}>
-        <div className={styles.imageWrapper}>
+      <div className={styles.bottomSection}>
+        <div>
+          <div className={styles.textWrapper}>
+            <div className={styles.textWithIcon}>
+              <Image
+                src="/assets/calendar-icon.svg"
+                width={16}
+                height={16}
+                alt="ikona kalendarza"
+              />
+              <p>
+                I - 10-16.02.2024
+                <br />
+                II - 17-23.02.2024
+              </p>
+            </div>
+            <div className={styles.textWithIcon}>
+              <Image
+                src="/assets/pin-icon.svg"
+                width={16}
+                height={16}
+                alt="ikona pinezki"
+              />
+              <p>🇵🇱 Czarna Góra, Region Masyw Śnieżnika</p>
+            </div>
+            <div className={styles.textWithIcon}>
+              <Image
+                src="/assets/group-icon.svg"
+                width={16}
+                height={16}
+                alt="grupa ludzi"
+              />
+              <p>
+                dzieci i młodzież <br /> w wieku 7 – 16 lat
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
           <Image
             src="/assets/events-camp-photo.png"
             width={202}
             height={150}
             alt="dzieci na stoku narciarskim"
           />
-        </div>
-        <div className={styles.textWrapper}>
-          <h3 className={styles.heading}>Zimowisko - Czarna Góra</h3>
-          <p>
-            <span className={styles.bold}>Terminy:</span> I - 10-16.02.2024 oraz
-            II - 17-23.02.2024
-          </p>
-          <p>
-            <span className={styles.bold}>Miejsce:</span> 🇵🇱 Czarna Góra, Region
-            Masyw Śnieżnika
-          </p>
-          <p>
-            <span className={styles.bold}>Uczestnicy:</span> dzieci i młodzież w
-            wieku 7 – 16 lat
-          </p>
         </div>
       </div>
     </div>
