89dbebcc3bcb43957d2129f8fb79dbc76ac03292
kacper-dabrowski <kdtw301@gmail.com>
2024-04-23 16:48:53 +0200

    fix(fonts, images): fix issues related to fonts and images

--- src/fonts/inter.ts
+++ src/fonts/inter.ts
@@ -1,3 +1,7 @@
 import { Inter } from "next/font/google";
 
-export const interFont = Inter({ subsets: ["latin"], weight: ["400", "500"] });
+export const interFont = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  display: "swap",
+});
