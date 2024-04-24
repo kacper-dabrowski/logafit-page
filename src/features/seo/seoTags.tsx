61cf92e098627b42733dd0b9f5f8d2d48d2301f2
kacper-dabrowski <kdtw301@gmail.com>
2024-04-24 17:37:32 +0200

    fix(seo): add meta tags

---
+++ src/features/seo/seoTags.tsx
@@ -0,0 +1,31 @@
+export function SeoTags() {
+  return (
+    <>
+      <title>
+        Logafit Centrum zdrowia i fitnessu - Szkoła pływania - Nauka pływania
+        Toruń
+      </title>
+      <meta
+        name="description"
+        content="Centrum zdrowia i fitnessu w Logafit zaprasza na zajęcia ruchowe w wodzie. Pływanie niemowląt, pływanie dzieci i młodzieży i wiele więcej. Zajęcia prowadzimy w Toruniu. Nauka pływania prowadzona przez profesjonalnych instruktorów. Lekcje pływania dla niemowląt, dzieci i młodzieży"
+      />
+      <meta
+        property="og:description"
+        content="Centrum zdrowia i fitnessu w Logafit zaprasza na zajęcia ruchowe w wodzie. Pływanie niemowląt, pływanie dzieci i młodzieży i wiele więcej. Zajęcia prowadzimy w Toruniu. Nauka pływania prowadzona przez profesjonalnych instruktorów. Lekcje pływania dla niemowląt, dzieci i młodzieży"
+      />
+      <meta
+        property="og:title"
+        content="Logafit Centrum zdrowia i fitnessu - Szkoła pływania - Nauka pływania Toruń"
+      />
+      <meta
+        property="article:modified_time"
+        content="2023-08-22T09:19:06+00:00"
+      />
+      <meta property="og:site_name" content="Logafit" />
+      <meta property="og:url" content="http://www.logafit.pl/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="pl_PL" />
+      <link rel="canonical" href="http://www.logafit.pl/" />
+    </>
+  );
+}
