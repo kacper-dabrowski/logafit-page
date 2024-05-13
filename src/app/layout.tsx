c78d5938849e8cf223daaac5d2ac856a0484ba15
kacper-dabrowski <kdtw301@gmail.com>
2024-05-13 14:41:00 +0200

    feat: add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./index.scss";
 import styles from "./layout.module.scss";
 import { Footer } from "../layout/footer/footer";
 import { SeoTags } from "../features/seo/seoTags";
+import { Header } from "../layout/header/header";
 
 export default function RootLayout({
   children,
@@ -13,11 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const classNames = clsx(interFont.className);
+
   return (
-    <html lang="en">
+    <html lang="pl">
       <SeoTags />
       <body className={classNames}>
         <Navbar additionalClasses={styles.navbar} />
+        <Header />
         <main className={styles.content}>{children}</main>
         <Footer />
         <SpeedInsights />
---
+++ src/layout/header/header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "./header.module.scss";
+
+export function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Image
+          src="/assets/header.svg"
+          width={1000}
+          height={500}
+          alt="nagłówek"
+        />
+      </div>
+      <div className={styles.headerMobile}>
+        <Image
+          src="/assets/header_mobile.svg"
+          width={768}
+          height={500}
+          alt="nagłówek mobilny"
+        />
+      </div>
+    </div>
+  );
+}
