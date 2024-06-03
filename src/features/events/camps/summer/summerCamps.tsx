e743ddc8db95f6ba0d0e223c49c73c82debbe301
kacper-dabrowski <kdtw301@gmail.com>
2024-06-04 00:05:43 +0200

    feat(events): add new variant for card

--- src/features/events/camps/summer/summerCamps.tsx
+++ src/features/events/camps/summer/summerCamps.tsx
@@ -1,3 +1,26 @@
+import { EventCard } from "../components/card/card";
+
 export function SummerCamps() {
-  return <div>Letnie obozy i kolonie</div>;
+  return (
+    <EventCard
+      title="Półkolonie pływackie"
+      imageSrc="/assets/kids-having-fun.png"
+      imageAlt="dzieci z napojami"
+      renderDates={() => (
+        <p>
+          I - 3-7.07.2024
+          <br /> II - 17-21.07.2024
+        </p>
+      )}
+      renderLocation={() => (
+        <p>Hotel Copernicus Toruń; Bulwar Filadelfijski 11</p>
+      )}
+      renderAge={() => (
+        <p>
+          dzieci <br /> w wieku 6 – 12 lat
+        </p>
+      )}
+      type="summer"
+    />
+  );
 }
