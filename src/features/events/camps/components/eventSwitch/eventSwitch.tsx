2c4147691982542c64a444165e5609fa9a885a9c
kacper-dabrowski <kdtw301@gmail.com>
2024-06-16 15:40:48 +0200

    fix(icons): fix icons colors and camps card

--- src/features/events/camps/components/eventSwitch/eventSwitch.tsx
+++ src/features/events/camps/components/eventSwitch/eventSwitch.tsx
@@ -1,35 +1,24 @@
 import clsx from "clsx";
 import styles from "./eventSwitch.module.scss";
-import { ResponsiveImage } from "../../../../shared/responsiveImage/responsiveImage";
 
 interface EventSwitchProps {
   variant: "yellow" | "blue";
-  iconSrc: string;
-  iconAltText: string;
+  renderIcon: () => JSX.Element;
   children: React.ReactNode;
   onClick: () => void;
 }
 
 export function EventSwitch({
   variant,
-  iconSrc,
+  renderIcon,
   children,
-  iconAltText,
   onClick,
 }: EventSwitchProps) {
   const containerClasses = clsx(styles.button, { [styles[variant]]: true });
-  const imageClasses = clsx(styles.icon);
 
   return (
     <button className={containerClasses} type="button" onClick={onClick}>
-      <ResponsiveImage
-        src={iconSrc}
-        width={{ mobile: 12, desktop: 24 }}
-        height={{ mobile: 12, desktop: 24 }}
-        alt={iconAltText}
-        mobileClassNames={imageClasses}
-        desktopClassNames={imageClasses}
-      />
+      <div>{renderIcon()}</div>
       <div>{children}</div>
     </button>
   );
