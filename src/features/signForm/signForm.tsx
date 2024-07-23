87d6b8bad354bb798f679a3a3dfdac180b6bcbaa
kacper-dabrowski <kdtw301@gmail.com>
2024-07-23 18:55:40 +0200

    feat(forms): submit forms in AJAX manner

--- src/features/signForm/signForm.tsx
+++ src/features/signForm/signForm.tsx
@@ -1,18 +1,56 @@
-import { PropsWithChildren } from "react";
+import { FormEventHandler, PropsWithChildren } from "react";
+import { Toaster, toast } from "react-hot-toast";
 import styles from "./signForm.module.scss";
 
 interface SignFormProps extends PropsWithChildren {}
 
 export function SignForm({ children }: SignFormProps) {
+  const onFormSubmit: FormEventHandler = async (event) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+
+    const data = formDataToObject(new FormData(form));
+
+    await toast.promise(submitForm(data, form), {
+      error: "Nie udało się zapisać na zajęcia, spróbuj ponownie później.",
+      loading: "Zapisuję...",
+      success: "Pomyślnie zapisano na zajęcia.",
+    });
+  };
   return (
     <div className={styles.container}>
-      <form
-        className={styles.form}
-        method="POST"
-        action="https://submit-form.com/hf6EMLT5W"
-      >
+      <form onSubmit={onFormSubmit} className={styles.form}>
         {children}
       </form>
+      <Toaster />
     </div>
   );
+}
+
+function formDataToObject(data: FormData) {
+  return Array.from(data.entries()).reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [key]: value,
+    };
+  }, {});
+}
+
+async function submitForm(data: Record<string, any>, form: HTMLFormElement) {
+  const url = "https://submit-form.com/hf6EMLT5W";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    return;
+  }
+
+  form.reset();
 }
