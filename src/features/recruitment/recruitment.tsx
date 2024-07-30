6e80cce5be0e87e2825ed5d706727d04fea9e2ee
kacper-dabrowski <kdtw301@gmail.com>
2024-07-30 23:19:30 +0200

    fix: fix card colors and contact info

--- src/features/recruitment/recruitment.tsx
+++ src/features/recruitment/recruitment.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { SecondaryHeader } from "../shared/typography/headers";
 import { RecruitmentCard } from "./card/card";
 import styles from "./recruitment.module.scss";
@@ -5,6 +7,8 @@ import olenderPhoto from "../../../public/assets/olender.png";
 import bazynskichPhoto from "../../../public/assets/bazynskich.png";
 import copernicusHotelPhoto from "../../../public/assets/hotel_copernicus.png";
 import halleraPhoto from "../../../public/assets/basen_hallera.avif";
+import phoneContact from "../../../public/assets/recruitment_contact_phone.png";
+import emailContact from "../../../public/assets/recruitment_contact_mail.png";
 
 export function Recruitment() {
   return (
@@ -19,17 +23,17 @@ export function Recruitment() {
         <RecruitmentCard
           image={copernicusHotelPhoto}
           name="Basen Hotel Copernicus"
-          variant="yellow"
+          variant="lightBlue"
         />
         <RecruitmentCard
           image={bazynskichPhoto}
           name="Aqua Toruń Bażyńskich"
-          variant="lightBlue"
+          variant="red"
         />
         <RecruitmentCard
           image={olenderPhoto}
           name="Olender Wielka Nieszawka"
-          variant="red"
+          variant="green"
         />
         <RecruitmentCard
           image={halleraPhoto}
@@ -38,6 +42,24 @@ export function Recruitment() {
         />
       </div>
       <h2 className={styles.secondLevelHeading}>Skontaktuj się z nami</h2>
+      <div className={styles.contact}>
+        <Link href="tel:607880086" className={styles.contactLink}>
+          <p className={styles.contactText}>Telefonicznie</p>
+          <Image
+            className={styles.contactImage}
+            src={phoneContact}
+            alt="607 880 086 lub 691 376 287"
+          />
+        </Link>
+        <Link href="mailto:biuro@logafit.pl" className={styles.contactLink}>
+          <p className={styles.contactText}>Mailowo</p>
+          <Image
+            src={emailContact}
+            alt="biuro@logafit.pl"
+            className={styles.contactImage}
+          />
+        </Link>
+      </div>
     </div>
   );
 }
