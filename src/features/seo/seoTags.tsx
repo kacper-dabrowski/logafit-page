9a3ac0451abcb96db299dfa90f78680bddbe9e9c
kacper-dabrowski <kdtw301@gmail.com>
2024-08-08 19:31:45 +0200

    feat(seo): improve seo descriptions

--- src/features/seo/seoTags.tsx
+++ src/features/seo/seoTags.tsx
@@ -2,16 +2,15 @@ export function SeoTags() {
   return (
     <>
       <title>
-        Logafit Centrum zdrowia i fitnessu - Szkoła pływania - Nauka pływania
-        Toruń
+        Logafit Toruń - Centrum Zdrowia i Fitness | Szkoła i Nauka Pływania
       </title>
       <meta
         name="description"
-        content="Centrum zdrowia i fitnessu w Logafit zaprasza na zajęcia ruchowe w wodzie. Pływanie niemowląt, pływanie dzieci i młodzieży i wiele więcej. Zajęcia prowadzimy w Toruniu. Nauka pływania prowadzona przez profesjonalnych instruktorów. Lekcje pływania dla niemowląt, dzieci i młodzieży"
+        content="Centrum Logafit w Toruniu oferuje zajęcia w wodzie: pływanie dla niemowląt, dzieci i młodzieży. Profesjonalni instruktorzy. Zapraszamy!"
       />
       <meta
         property="og:description"
-        content="Centrum zdrowia i fitnessu w Logafit zaprasza na zajęcia ruchowe w wodzie. Pływanie niemowląt, pływanie dzieci i młodzieży i wiele więcej. Zajęcia prowadzimy w Toruniu. Nauka pływania prowadzona przez profesjonalnych instruktorów. Lekcje pływania dla niemowląt, dzieci i młodzieży"
+        content="Centrum Logafit w Toruniu oferuje zajęcia w wodzie: pływanie dla niemowląt, dzieci i młodzieży. Profesjonalni instruktorzy. Zapraszamy!"
       />
       <meta
         property="og:title"
@@ -19,13 +18,13 @@ export function SeoTags() {
       />
       <meta
         property="article:modified_time"
-        content="2023-08-22T09:19:06+00:00"
+        content="2024-08-08T09:19:06+00:00"
       />
       <meta property="og:site_name" content="Logafit" />
-      <meta property="og:url" content="http://www.logafit.pl/" />
+      <meta property="og:url" content="https://www.logafit.pl/" />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="pl_PL" />
-      <link rel="canonical" href="http://www.logafit.pl/" />
+      <link rel="canonical" href="https://www.logafit.pl/" />
     </>
   );
 }
