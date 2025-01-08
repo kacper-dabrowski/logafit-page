0fb2333f2c4a5f1d7305a570b3eb67e1e023cddc
DarthRevan77797 <general77779@gmail.com>
2025-01-08 22:01:35 +0100

    test(infobox test): LF-12 add test (snapshot)

--- src/features/whereAreWe/infoBox/infoBox.test.tsx
+++ src/features/whereAreWe/infoBox/infoBox.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import { InfoBox } from "./infoBox";
 
 describe("infoBox section", () => {
-  it("should render the infoBox section that display text based one array ,array is created from text split based on separator", () => {
+  it("displays infoBox text correcly ", () => {
     render(
       <InfoBox
         text="607 880 086 oraz 691 376 287"
@@ -14,5 +15,18 @@ describe("infoBox section", () => {
     expect(screen.getByText("607 880 086")).toBeInTheDocument();
     expect(screen.getByText("oraz")).toBeInTheDocument();
     expect(screen.getByText("691 376 287")).toBeInTheDocument();
+  });
+
+  it("injects a separator in a separate element", () => {
+    const domTree = renderer
+      .create(
+        <InfoBox
+          text="607 880 086 oraz 691 376 287"
+          separator="oraz"
+          variant="green"
+        />,
+      )
+      .toJSON();
+    expect(domTree).toMatchSnapshot();
   });
 });
