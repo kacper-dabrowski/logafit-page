d322b78df35c5444cc34ec59fd59fff1f4fbf866
kacper-dabrowski <kdtw301@gmail.com>
2025-05-16 18:46:31 +0200

    feat(sitemap): consolidate event sitemap generation into main sitemap file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,18 @@
 import { MetadataRoute } from "next";
+import { eventTransformer } from "../features/contentful/events/event.transformer";
+import {
+  AvailableEntity,
+  contentfulClientService,
+} from "../features/contentful/service/service";
 
 const BASE_URL = "https://logafit.pl";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return [
+  const events = eventTransformer.transformMany(
+    await contentfulClientService.getEntries(AvailableEntity.Events),
+  );
+
+  const staticPages = [
     {
       url: `${BASE_URL}/`,
       lastModified: new Date().toISOString(),
@@ -21,4 +30,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date().toISOString(),
     },
   ];
+
+  const eventPages = events.map((event) => ({
+    url: `${BASE_URL}/wydarzenia/${event.slug}`,
+    lastModified: event.updatedAt,
+  }));
+
+  return [...staticPages, ...eventPages];
 }
