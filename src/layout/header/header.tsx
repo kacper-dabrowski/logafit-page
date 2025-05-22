ff50a786fbb174937d4ba00602bd2b97b2600d90
kacper-dabrowski <kdtw301@gmail.com>
2025-05-22 15:56:28 +0200

    chore(deps): update nextjs to v15

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,31 +1,38 @@
 import Image from "next/image";
+import desktopImage from "../../../public/assets/header.png";
+import mobileImage from "../../../public/assets/header_mobile.png";
 import styles from "./header.module.scss";
 
 export function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.background} />
-        <Image
-          src="/assets/header.png"
-          width={936}
-          height={500}
-          alt="nagłówek"
-          quality={100}
-          priority
-          className={styles.headerImage}
-        />
-      </div>
       <div className={styles.headerMobile}>
         <div className={styles.background} />
         <Image
-          src="/assets/header_mobile.png"
+          src={mobileImage}
           width={774}
           height={500}
           quality={100}
           alt="nagłówek mobilny"
           priority
           className={styles.headerImage}
+          sizes="(max-width: 1024px) 100vw, 774px"
+          placeholder="empty"
+        />
+      </div>
+
+      <div className={styles.header}>
+        <div className={styles.background} />
+        <Image
+          src={desktopImage}
+          width={936}
+          height={500}
+          quality={100}
+          alt="nagłówek"
+          priority
+          className={styles.headerImage}
+          sizes="(max-width: 1024px) 100vw, 936px"
+          placeholder="empty"
         />
       </div>
     </div>
